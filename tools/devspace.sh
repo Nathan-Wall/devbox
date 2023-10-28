793eed21955fae2b3e24925910522b623eff0149
Nathan Wall <nathan@wall.coffee>
2023-10-28 20:49:10 +0000

    ++

--- tools/devspace.sh
+++ tools/devspace.sh
@@ -9,13 +9,16 @@ if [ "$1" == 'devbox' ]; then
   exit 0
 fi
 
-# Look for .devspace
-until [ -e .devspace ] || [ "$(pwd)" == '/' ]; do
+# Look for .devspace or .forjespace
+until [ -e .devspace ] || [ -e .forjespace ] || [ $(pwd) == '/' ]; do
   cd ..
 done
-if [ -e .devspace ]; then
-  echo "$(pwd)"
+if [ -e .devspace ] || [ -e .forjespace ]; then
+  space="$(pwd)"
+  export FORJESPACE="$space"
+  echo "$space"
 elif [ "$1" != '--nofail' ]; then
   echo 'No dev workspace found in parent directories.' >&2
   exit 1
 fi
+
